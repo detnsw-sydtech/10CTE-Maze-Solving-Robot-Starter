8555ec698b732492e1e8e74f7b3c0ef189e5cd0b
David Hawkes <david.hawkes11@det.nsw.edu.au>
2026-08-01 22:54:38 +1000

    Add utility functions header file

---
+++ src/utils.h
@@ -0,0 +1,11 @@
+// =====================================================
+//  Utility Functions Header
+// =====================================================
+
+#ifndef UTILS_H
+#define UTILS_H
+
+void initUtils();
+void debugPrint(bool leftWall, bool rightWall, long frontDist);
+
+#endif
