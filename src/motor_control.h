b56a6464e710f3d8290474bca7ae5b8aea197245
David Hawkes <david.hawkes11@det.nsw.edu.au>
2026-08-01 22:51:27 +1000

    Add motor control header file with function declarations

---
+++ src/motor_control.h
@@ -0,0 +1,15 @@
+// =====================================================
+//  Motor Control Header
+// =====================================================
+
+#ifndef MOTOR_CONTROL_H
+#define MOTOR_CONTROL_H
+
+void initMotors();
+void forward();
+void turnLeft();
+void turnRight();
+void turnAround();
+void stopRobot();
+
+#endif
